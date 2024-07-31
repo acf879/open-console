0a493f625cc46b9b0073fd836035bf6de91d9770
Ashton Fritz <Ashton@adaptiveagriculture.ca>
2024-07-31 09:16:57 -0600

    Added initial starting point for func defs
    Also fixed the header file path

--- system_base/src/game_object.c
+++ system_base/src/game_object.c
@@ -1,1 +1,22 @@
-#include "game_object.h"
+#include <stdbool.h>
+#include "../game_engine/game_object.h"
+
+bool set_tag(Game_Object obj, const Tag tag) {
+  return true;
+}
+bool set_hit_box(Game_Object obj, const Hit_Box hit_box, const size_t hit_box_size) {
+  return true;
+}
+bool set_group(Game_Object obj, const Group group) {
+  return true;
+}
+bool set_is_trigger(Game_Object obj, const bool is_trigger) {
+  return true;
+}
+bool is_trigger(const Game_Object obj) {
+  return true;
+}
+bool is_colliding(const Game_Object object_1, const Game_Object object_2) {
+  return true;
+}
+
